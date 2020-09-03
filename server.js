220408a49fe2d2fe06bf189dd79dde228546745c
Dan Wendorf <dan@render.com>
2020-09-03 10:51:23 -0700

    Bump dependencies
    * Migrate from deprecated `hapi` and `inert` to `@hapi/hapi` and
      `@hapi/inert`.
    * Bump all versions to the latest.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const Hapi = require("hapi");
+const Hapi = require("@hapi/hapi");
 
 const server = Hapi.server({
   port: 3000,
@@ -31,7 +31,7 @@ const init = async () => {
 
   await server.register([
     {
-      plugin: require("inert"),
+      plugin: require("@hapi/inert"),
       options: {}
     },
     {
