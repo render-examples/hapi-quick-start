b73a1f2ff278c575ebda2c4709655f67d967373c
Jason Leveille <jason@leve.us>
2022-07-18 21:24:50 -0400

    wip

--- server.js
+++ server.js
@@ -9,20 +9,9 @@ const server = Hapi.server({
 
 server.route({
   method: "GET",
-  path: "/{path*}",
+  path: "{path*}",
   handler: (request, h) => {
-    return h.redirect('https://auth-qa.clarityapp.com' + request.params.path).code(301);
-  }
-});
-
-server.route({
-  method: "GET",
-  path: "/{name}",
-  handler: (request, h) => {
-
-    request.logger.info("In handler %s", request.path);
-
-    return `Hello, ${encodeURIComponent(request.params.name)}!`;
+    return h.redirect(process.env.REDIRECT_TO + request.params.path).code(301);
   }
 });
 
